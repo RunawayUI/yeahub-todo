989d28563d28d0a6081b65dbc17e165aec27b6a4
RunawayUI <tosha.grafkof@yandex.ru>
2026-09-15 22:55:53 +0300

    feat: add task creation

--- script.js
+++ script.js
@@ -13,17 +13,61 @@ todoForm.classList.add('todo-form');
 todoForm.setAttribute('action', '#');
 todoForm.innerHTML = `
   <label for="task-name">Задача</label>
-  <input type="text" name="task-name" required>
+  <input type="text" name="task-name" id="task-name" required>
 
   <label for="deadline">Дедлайн</label>
-  <input type="date" name="deadline" required>
+  <input type="date" name="deadline" id="deadline" required>
 
   <label for="person-in-charge">Ответственный</label>
-  <input type="text" name="person-in-charge" required>
+  <input type="text" name="person-in-charge" id="person-in-charge" required>
 
-  <button type="submit">Добавить</button>
+  <button type="submit" id="submit-btn">Добавить</button>
 `;
 todoContainer.appendChild(todoForm);
 
+const taskList = document.createElement('ul');
+taskList.classList.add('task-list')
+todoContainer.appendChild(taskList);
 
+const taskName = document.getElementById('task-name');
+const deadline = document.getElementById('deadline');
+const personInCharge = document.getElementById('person-in-charge');
+
+todoForm.addEventListener('submit', (event) => {
+  if (!taskName.value || !deadline.value || !personInCharge.value) {
+    event.preventDefault();
+    alert('Заполните все поля!');
+  } else {
+    event.preventDefault();
+
+    const taskItem = document.createElement('li');
+    taskItem.classList.add('task-item');
+    taskItem.innerHTML = `
+      <input type="checkbox" name="task-checkbox">
+      <div><p><b>Задача:</b></p> ${taskName.value}</div>
+      <div><p><b>Дедлайн:</b></p> ${deadline.value}</div>
+      <div><p><b>Ответственный:</b></p> ${personInCharge.value}</div>
+    `;
+    
+    taskList.appendChild(taskItem);
+    toggleTaskCompletion(taskItem);
+    clearInputs(todoForm);
+  }
+});
+
+function toggleTaskCompletion(element) {
+  const checkbox = element.querySelector('input');
+
+  checkbox.addEventListener('change', () => {
+    element.classList.toggle('completed');
+  })
+}
+
+function clearInputs(form) {
+  const inputs = form.querySelectorAll('input');
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+}
 
